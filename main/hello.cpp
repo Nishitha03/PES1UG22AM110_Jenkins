8a10fecdbb97bf480eb4a07b86f05ccb91bd9521
Nishitha03 <128141637+Nishitha03@users.noreply.github.com>
2025-03-13 12:20:57 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this from the new build less go \n";
+  cout << "I have successfully built and run this from the new build leswwwwws go \n";
   return 0;
 }
